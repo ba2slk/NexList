9f3309defe4fc635bff1dc00764f069ca4e4b7c5
ba2slk <0730bss@gmail.com>
2025-06-02 19:36:59 +0900

    loki config

--- nexlist-app/main.py
+++ nexlist-app/main.py
@@ -12,6 +12,14 @@ import os
 from prometheus_fastapi_instrumentator import Instrumentator
 from influxdb import InfluxDBClient
 
+import logging
+import time
+from multiprocessing import Queue
+from os import getenv
+from fastapi import Request
+from logging_loki import LokiQueueHandler
+
+
 # Constants
 JSON_PATH = "./todo.json"
 TODO_NOT_FOUND_DETAIL = "Todo not found"
@@ -96,6 +104,35 @@ app.add_middleware(
 
 # Prometheus Metrics Endpoint (/metrics)
 Instrumentator().instrument(app).expose(app, endpoint="/metrics")
+
+loki_logs_handler = LokiQueueHandler(
+    Queue(-1),
+    url=getenv("LOKI_ENDPOINT"),
+    tags={"application": "fastapi"},
+    version="1",
+)
+
+# Custom access logger (ignore Uvicorn's default logging)
+custom_logger = logging.getLogger("custom.access")
+custom_logger.setLevel(logging.INFO)
+
+# Add Loki handler (assuming `loki_logs_handler` is correctly configured)
+custom_logger.addHandler(loki_logs_handler)
+
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time  # Compute response time
+
+    log_message = (
+        f'{request.client.host} - "{request.method} {request.url.path} HTTP/1.1" {response.status_code} {duration:.3f}s'
+    )
+
+    # **Only log if duration exists**
+    if duration:
+        custom_logger.info(log_message)
+
+    return response
 
 
 # Template
