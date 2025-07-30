fa0dd0a62d23c815796f367c6bff1b7f7f788b50
ba2slk <0730bss@gmail.com>
2025-07-30 23:54:13 +0900

    Edit: black 포맷팅

--- app/src/todos/models.py
+++ app/src/todos/models.py
@@ -1,7 +1,7 @@
 # 데이터 구조만 정의
 
-from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean, Date
+from src.database import Base
+from sqlalchemy import Boolean, Column, Date, Integer, String
 
 
 # Base: DB 테이블 정의 선언
@@ -9,6 +9,6 @@ class Todo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    isDone = Column(Boolean, default=False)
+    is_done = Column(Boolean, default=False)
     task = Column(String(255))
     due_date = Column(Date)
