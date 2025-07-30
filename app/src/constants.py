a4fe5e7754e074aaa4039270d69241f14e4f1a56
ba2slk <0730bss@gmail.com>
2025-07-30 23:40:30 +0900

    Add: src/ 내 글로벌 모듈 위치 변경 및 추가

---
+++ app/src/constants.py
@@ -0,0 +1,14 @@
+# Global Constants
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Constants for MySQL connection
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DB_NAME = os.getenv("MYSQL_DB_NAME")
---
+++ app/src/main.py
@@ -0,0 +1,27 @@
+from database import Base, engine
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from todos.models import Todo
+from todos.router import router as todos_router
+
+# Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)  # Base에 연결된 모든 Table 생성
+
+# FastAPI Configuration
+app = FastAPI(debug=True)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# posts_router
+app.include_router(todos_router)
+
+# CORS 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
