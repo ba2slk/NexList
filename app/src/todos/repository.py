b640db976143e8a6f8b954793350daedafe01fc7
ba2slk <0730bss@gmail.com>
2025-08-07 19:06:46 +0900

    Refactor: TodoRepository 생성자에 db 세션을 주입하도록 구조 개선

--- app/src/todos/repository.py
+++ app/src/todos/repository.py
@@ -11,98 +11,102 @@ from todos.schemas import TodoCompletedState, TodoItem
 # Todo Interface
 class TodoRepositoryInterface:
     @abstractmethod
-    def get_todo_by_id(self, todo_id: int, user_id: int, db: Session) -> Todo | None:
+    def get_todo_by_id(self, todo_id: int, user_id: int) -> Todo | None:
         pass
 
     @abstractmethod
-    def get_all_todos(self, user_id: int, db: Session) -> list[Todo]:
+    def get_all_todos(self, user_id: int) -> list[Todo]:
         pass
 
     @abstractmethod
-    def create_todo(self, todo: TodoItem, user_id: int, db: Session) -> Todo:
+    def create_todo(self, todo: TodoItem, user_id: int) -> Todo:
         pass
 
     @abstractmethod
-    def remove_all_todos(self, user_id: int, db: Session) -> int:
+    def remove_all_todos(self, user_id: int) -> int:
         pass
 
     @abstractmethod
-    def remove_todo_by_id(todo_id: int, user_id: int, db: Session):
+    def remove_todo_by_id(todo_id: int, user_id: int):
         pass
 
     @abstractmethod
     def update_todo_by_id(
-        todo_id: int, change: TodoItem, user_id: int, db: Session
+        todo_id: int, change: TodoItem, user_id: int
     ) -> Todo:
         pass
 
     @abstractmethod
     def update_completed_state_by_id(
-        todo_id: int, state: TodoCompletedState, user_id: int, db: Session
+        todo_id: int, state: TodoCompletedState, user_id: int
     ) -> Todo:
         pass
 
 
 # TodoRepository 구현체
 class TodoRepository(TodoRepositoryInterface):
+    def __init__(self, db: Session):
+        self.db = db
+
+
     # READ: single todo
-    def get_todo_by_id(self, todo_id: int, user_id: int, db: Session) -> Todo | None:
+    def get_todo_by_id(self, todo_id: int, user_id: int) -> Todo | None:
         return (
-            db.query(Todo).filter(Todo.id == todo_id, Todo.user_id == user_id).first()
+            self.db.query(Todo).filter(Todo.id == todo_id, Todo.user_id == user_id).first()
         )
 
     # READ: all todos
-    def get_all_todos(self, user_id: int, db: Session) -> list[Todo] | None:
-        return db.query(Todo).filter(Todo.user_id == user_id).all()
+    def get_all_todos(self, user_id: int) -> list[Todo] | None:
+        return self.db.query(Todo).filter(Todo.user_id == user_id).all()
 
     # CREATE: single todo
-    def create_todo(self, todo: TodoItem, user_id: int, db: Session) -> Todo:
+    def create_todo(self, todo: TodoItem, user_id: int) -> Todo:
         new_todo = Todo(**todo.model_dump())
         new_todo.user_id = user_id
-        db.add(new_todo)
-        db.commit()
-        db.refresh(new_todo)
+        self.db.add(new_todo)
+        self.db.commit()
+        self.db.refresh(new_todo)
         return new_todo
 
     # DELETE: all todos
-    def remove_all_todos(self, user_id: int, db: Session):
-        db.query(Todo).filter(Todo.user_id == user_id).delete()
-        db.commit()
+    def remove_all_todos(self, user_id: int):
+        self.db.query(Todo).filter(Todo.user_id == user_id).delete()
+        self.db.commit()
 
     # DELETE: single todo
     def remove_todo_by_id(
-        self, todo_id: int, user_id: int, db: Session
+        self, todo_id: int, user_id: int
     ) -> int | None:
-        todo = self.get_todo_by_id(todo_id, user_id, db)
+        todo = self.get_todo_by_id(todo_id, user_id)
         if not todo:
             return None
         deleted = (
-            db.query(Todo).filter(Todo.id == todo_id, Todo.user_id == user_id).delete()
+            self.db.query(Todo).filter(Todo.id == todo_id, Todo.user_id == user_id).delete()
         )
-        db.commit()
+        self.db.commit()
         return deleted
 
     # UPDATE: todo
     def update_todo_by_id(
-        self, todo_id: int, change: TodoItem, user_id: int, db: Session
+        self, todo_id: int, change: TodoItem, user_id: int
     ) -> Todo:
-        todo = self.get_todo_by_id(todo_id, user_id, db)
+        todo = self.get_todo_by_id(todo_id, user_id)
         if not todo:
             return None
         todo.task = change.task
         todo.due_date = change.due_date
-        db.commit()
-        db.refresh(todo)
+        self.db.commit()
+        self.db.refresh(todo)
         return todo
 
     def update_completed_state_by_id(
-        self, todo_id: int, state: TodoCompletedState, user_id: int, db: Session
+        self, todo_id: int, state: TodoCompletedState, user_id: int
     ) -> Todo:
-        todo = self.get_todo_by_id(todo_id, user_id, db)
+        todo = self.get_todo_by_id(todo_id, user_id)
         if not todo:
             return None
         todo.is_done = state.is_done
-        db.commit()
-        db.refresh(todo)
+        self.db.commit()
+        self.db.refresh(todo)
         return todo
     
--- app/src/todos/service.py
+++ app/src/todos/service.py
@@ -9,45 +9,44 @@ from fastapi import HTTPException
 
 
 class TodoService:
-    def __init__(self, repository: TodoRepositoryInterface, db: Session):
+    def __init__(self, repository: TodoRepositoryInterface):
         self.repository = repository
-        self.db = db
 
 
     def create_todo(self, todo: TodoItem, user: User) -> Todo:
-        return self.repository.create_todo(todo, user.id, self.db)
+        return self.repository.create_todo(todo, user.id)
     
 
     def get_all_todos(self, user: User) -> list[Todo]:
-        return self.repository.get_all_todos(user.id, self.db)
+        return self.repository.get_all_todos(user.id)
     
 
     def get_todo_by_id(self, id: int, user: User) -> Todo:
-        todo = self.repository.get_todo_by_id(id, user.id, self.db)
+        todo = self.repository.get_todo_by_id(id, user.id)
         if todo is None:
             raise HTTPException(status_code=404, detail=TODO_NOT_FOUND_DETAIL)
         return todo
     
 
     def remove_all_todos(self, user: User):
-        self.repository.remove_all_todos(user.id, self.db)
+        self.repository.remove_all_todos(user.id)
     
 
     def remove_todo_by_id(self, id: int, user: User):
-        deleted_todo = self.repository.remove_todo_by_id(id, user.id, self.db)
+        deleted_todo = self.repository.remove_todo_by_id(id, user.id)
         if deleted_todo is None:
             raise HTTPException(status_code=404, detail=TODO_NOT_FOUND_DETAIL)
 
 
     def update_todo_by_id(self, todo: TodoItem, id: int, user: User) -> Todo:
-        updated_todo = self.repository.update_todo_by_id(id, todo, user.id, self.db)
+        updated_todo = self.repository.update_todo_by_id(id, todo, user.id)
         if updated_todo is None:
             raise HTTPException(status_code=404, detail=TODO_NOT_FOUND_DETAIL)
         return updated_todo
 
 
     def update_todo_completed_state_by_id(self, id: int, update: TodoCompletedState, user: User) -> Todo:
-        updated_todo = self.repository.update_completed_state_by_id(id, update, user.id, self.db)
+        updated_todo = self.repository.update_completed_state_by_id(id, update, user.id)
         if updated_todo is None:
             raise HTTPException(status_code=404, detail=TODO_NOT_FOUND_DETAIL)
         return updated_todo
