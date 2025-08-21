72c110521b91d3349eee383039ee270fed4bdede
ba2slk <0730bss@gmail.com>
2025-08-21 11:45:26 +0900

    edit: app -> server로 디렉토리명 변경

--- app/src/memo/dependencies.py
+++
@@ -1,9 +0,0 @@
-from memo.repository import MemoRepository
-from memo.service import MemoService
-from fastapi import Depends
-from db.dependencies import get_db
-from sqlalchemy.orm import Session
-
-def get_memo_service(db: Session = Depends(get_db)) -> MemoService:
-    repo = MemoRepository(db)
-    return MemoService(repository = repo)
--- app/src/memo/models.py
+++
@@ -1,9 +0,0 @@
-from db.database import Base
-from sqlalchemy import Column, Date, Integer, String, ForeignKey
-
-
-class Memo(Base):
-    __tablename__ = "memos"
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
-    content = Column(String(8192))
-    saved_at = Column(Date)
