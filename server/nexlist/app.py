e8f84ce73e0f47bca26523b135612b73f5fceb34
ba2slk <0730bss@gmail.com>
2025-08-21 17:08:33 +0900

    BREAKING CHANGE(url): frontend host port 기존 5173 -> 7777 변경 및 cors 허용 url 수정

--- server/nexlist/app.py
+++ server/nexlist/app.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 
 from nexlist.auth.router import router as auth_router
 from nexlist.db.database import create_tables
@@ -13,7 +11,6 @@ create_tables()
 
 # FastAPI Configuration
 app = FastAPI(debug=True)
-app.mount("/assets", StaticFiles(directory="nexlist/static/dist/assets"), name="assets")
 
 # include routers
 app.include_router(todos_router)
@@ -23,13 +20,8 @@ app.include_router(memo_router)
 # CORS 허용
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000", "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000", "http://localhost:7777", "http://127.0.0.1:7777"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/{full_path:path}")
-async def serve_react_app(request: Request, full_path: str):
-    return FileResponse("static/dist/index.html")
